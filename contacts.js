7a3d69dff8b4d6669da523e5dde4921d7e0cad14
sfirsovdev <s.firsov1985@gmail.com>
2022-09-26 16:47:38 +0300

    FInal

--- contacts.js
+++ contacts.js
@@ -1,21 +1,43 @@
-/*
- * Раскомментируй и запиши значение
- * const contactsPath = ;
- */
+const { v4 } = require('uuid');
+const fs = require('fs').promises;
+const path = require('path');
 
-// TODO: задокументировать каждую функцию
-function listContacts() {
-  // ...твой код
+const contactsPath = path.resolve(__dirname, 'db/contacts.json');
+
+async function listContacts() {
+    const data = await fs.readFile(contactsPath, 'utf-8');
+    const contacts = data ? JSON.parse(data) : [];
+    return contacts;
 }
 
-function getContactById(contactId) {
-  // ...твой код
+async function getContactById(contactId) {
+    const contacts = await listContacts();
+    const contact = contacts.find(item => item.id === contactId);
+    return contact ? contact : null;
 }
 
-function removeContact(contactId) {
-  // ...твой код
+async function removeContact(contactId) {
+    const contacts = await listContacts();
+    const idx = contacts.findIndex(item => item.id === contactId);
+    if (idx === -1) {
+        return null;
+    }
+    const removedContact = contacts.splice(idx, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return removedContact;
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+async function addContact(name, email, phone) {
+    const contacts = await listContacts();
+    const newContact = { id: v4(), name, email, phone };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return newContact;
 }
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+};
--- index.js
+++ index.js
@@ -1,27 +1,49 @@
-const argv = require("yargs").argv;
+const { listContacts, getContactById, removeContact, addContact } = require('./contacts');
+const { Command } = require('commander');
+const program = new Command();
 
-// TODO: рефакторить
-function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      // ...
-      break;
+program
+    .option('-a, --action <type>', 'choose action')
+    .option('-i, --id <type>', 'user id')
+    .option('-n, --name <type>', 'user name')
+    .option('-e, --email <type>', 'user email')
+    .option('-p, --phone <type>', 'user phone');
 
-    case "get":
-      // ... id
-      break;
+program.parse(process.argv);
 
-    case "add":
-      // ... name email phone
-      break;
+const argv = program.opts();
 
-    case "remove":
-      // ... id
-      break;
+async function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+    case 'list':
+        const contacts = await listContacts();
+        console.log(contacts);
+        break;
+
+    case 'get':
+        const contact = await getContactById(id);
+        if (!contact) {
+        throw new Error(`Contact with id:${id} not found`);
+        }
+        console.log(contact);
+        break;
+
+    case 'add':
+        const newContact = await addContact(name, email, phone);
+        console.log(newContact);
+        break;
+
+    case 'remove':
+        const removedContact = await removeContact(id);
+        if (!removedContact) {
+        throw new Error(`Contact with id:${id} not found`);
+        }
+        console.log(removedContact);
+        break;
 
     default:
-      console.warn("\x1B[31m Unknown action type!");
-  }
+        console.warn('\x1B[31m Unknown action type!');
+    }
 }
 
 invokeAction(argv);
